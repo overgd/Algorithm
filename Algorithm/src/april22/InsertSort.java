65ab1263214e2a012537ddbe5055b9b0f9493593
Administrator <Administrator@Sc-201504131508>
2015-04-22 18:28:49 +0900

    0422

---
+++ Algorithm/src/april22/InsertSort.java
@@ -0,0 +1,10 @@
+package april22;
+
+public class InsertSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Algorithm/src/april22/RandomNumber.java
+++ Algorithm/src/april22/RandomNumber.java
@@ -1,5 +1,27 @@
 package april22;
 
 public class RandomNumber {
-
+	
+	int[] ball = new int[45];
+	int random_number;
+	int temp_number;
+		
+	void generate() {
+		
+		for(int i = 0;i < ball.length ;i++) {
+			ball[i] = i+1;
+		}
+		
+		for(int i = 0;i<100;i++){
+			random_number = (int)(Math.random()*44+1);
+			temp_number = ball[0];
+			ball[0] = ball[random_number];
+			ball[random_number] = temp_number;
+		}
+		
+//		for(int i = 0;i<6;i++){
+//			System.out.println(ball[i]);
+//		}
+		
+	}
 }
